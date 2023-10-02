d5399d6a25e925ecb662b2b41989f3ec2270b7ce
Nitesh Kumar Agrahari <niteshjnt@gmail.com>
2023-10-02 17:42:51 +0530

    first commit

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_injestion import DataIngestion
+from src.components.model_trainer import ModelTrainer
+from src.utils import evaluate_model
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
